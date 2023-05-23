cdc73906702f7e9728c20c741e33eeece7dfdd50
abonilla <aeh.bonilla@gmail.com>
2023-05-23 15:27:51 -0600

    modify App.js to test GithubActions deployment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+        Hola mundo! Mi nombre es Alain :D
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
