ddeb5831cdf3200676039a634821b2675a934d1d
Alain Bonilla <38193259+aeh-bonilla@users.noreply.github.com>
2023-05-30 16:35:28 -0600

    Update App.js
    add grettings to the crew.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-        Hola mundo! Mi nombre es Alain :D, Hola Ornito!
+        Hola mundo! Mi nombre es Alain :D, Hola Ornito! Saludos a nuestros amigos ETECS.
         </p>
         <a
           className="App-link"
