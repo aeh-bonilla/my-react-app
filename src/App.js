8c6787836200b9da421e3c300db617714c5a6227
alain-at-669096690527 <aeh.bonilla@gmail.com>
2025-02-26 22:09:45 -0600

    gretting message changed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-        Hola Grupo de AWS Puebla, esta es una prueba 5!
+        Deployment exitoso!
         </p>
         <a
           className="App-link"
